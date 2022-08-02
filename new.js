71e85a120df262fcf0a5a5f4aba4f362af736739
shaik sadiq <ssadiq.skt@gmail.com>
2022-08-02 16:16:20 +0530

    learning java script

---
+++ new.js
@@ -0,0 +1,14 @@
+console.log('hello world my name is sadiq iam here to learn JS.')
+alert('test')
+
+document.getElementById("new").innerHTML=5+6;
+let x,y,z;
+x=8;
+y=9;
+z=x+y;
+// document.getElementById("demo").innerHTML =
+//     `The value of z is ${z}.`;  
+
+function newFunction() {
+    return "The value of z is ";
+}
