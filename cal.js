5ab27ea438d63e76f71054972f7ff3af618b53bf
shaik sadiq <ssadiq.skt@gmail.com>
2022-08-03 00:21:11 +0530

    learninh css cal

---
+++ cal.js
@@ -0,0 +1,18 @@
+console.log("hello world")
+  
+
+let display= document.getElementById('display');
+
+
+let buttons= document.getElementById('click');
+
+
+
+
+
+
+
+// line-height: 100px;
+
+
+// text-align: right;
