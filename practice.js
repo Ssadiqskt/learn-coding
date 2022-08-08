d25b70bdcbfe6fde652adf7ec41d518ee0531196
shaik sadiq <ssadiq.skt@gmail.com>
2022-08-09 00:17:27 +0530

    learning conditions in JS

--- practice.js
+++ practice.js
@@ -1,3 +1,78 @@
 let a= "sadiq";
 let b= 9;
 console.log(a+b);
+const a1 = {
+    name: "sadiq",
+    section: "B",
+    pass: true
+}
+a1['name'] = "saqeeb",
+console.log(a1);
+
+const dist = {
+    hello : "way to talk",
+    hi : "greetig",     
+}
+console.log(dist.hi)
+
+/*----------operators and expressions------*/
+
+ let a2 = 50 , b2 = 10;
+console.log("a2++",a2++);
+console.log("++a2",++a2);
+console.log("a2--",a2--);
+console.log("--a2",--a2);
+/*--------assignment operators------
++=,
+=+,
+-=,
+=-,
+*=,
+***/
+let a3 = 10;
+a+=5
+console.log(a3)
+
+
+/*-----comparision operators------------------*/
+
+let comp1 = 6;
+let comp2 =8;
+console.log("comp1 == comp2 is", comp1==comp2)
+console.log("comp1 == comp2 is", comp1!=comp2)
+console.log("comp1 === comp2 is", comp1===comp2)
+console.log("comp1 !== comp2 is", comp1!==comp2)
+
+/*-------logical operators------*/
+
+let x=8;
+let x1= 9;
+console.log(x<x1 && x==x1-1)
+console.log(x>x1 || x==x1-1)
+console.log(!false)
+
+/*-------conditional operators------*/
+let p =prompt("how old are you");
+let p2 = prompt("Are you married?")
+if (p>=24 && p2=="yes") {
+    alert("you r marrried at the correct age!");
+} else if(p<24 && p2=="no") {
+    alert("wait till 24")
+}
+ else if(p>24 && p2=="no"){
+    alert("you should get married")
+    
+}
+else if(p<24 && p2=="yes"){
+    alert("you should have waited till 24")
+    
+}
+    
+
+
+
+
+
+
+
+
