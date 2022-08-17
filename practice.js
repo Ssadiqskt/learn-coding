1b6acba4eed54d8af838269cd4576b02bd25e838
shaik sadiq <ssadiq.skt@gmail.com>
2022-08-17 19:02:54 +0530

    practicre js

--- practice.js
+++ practice.js
@@ -52,27 +52,52 @@ console.log(x>x1 || x==x1-1)
 console.log(!false)
 
 /*-------conditional operators------*/
-let p =prompt("how old are you");
-let p2 = prompt("Are you married?")
-if (p>=24 && p2=="yes") {
-    alert("you r marrried at the correct age!");
-} else if(p<24 && p2=="no") {
-    alert("wait till 24")
-}
- else if(p>24 && p2=="no"){
-    alert("you should get married")
+// let p =prompt("how old are you");
+// let p2 = prompt("Are you married?")
+// if (p>=24 && p2=="yes") {
+//     alert("you r marrried at the correct age!");
+// } else if(p<24 && p2=="no") {
+//     alert("wait till 24")
+// }
+//  else if(p>24 && p2=="no"){
+//     alert("you should get married")
+    
+// }
+// else if(p<24 && p2=="yes"){
+//     alert("you should have waited till 24")
+    
+// }
+    
+/*---------for loop-----------*/
+// let mul = 1
+// let n = prompt("value");
+//  n = Number.parseInt(n);
+//  for(let i = 1; i<n; i++){
+//     console.log(i)
+//     mul += (i)
+//  }
+ 
+
+// console.log("multiply of " + n + "is" +mul)
+
+// /*-----------while loop---------*/
+// let m = 0
+// while(m<50){
+//     console.log(m);
+//     m++;
+// }
+
+
+
+let marks = {
+    harry: 55,
+    sid: 60,
+    sam: 65,
+    shaym:78,
     
 }
-else if(p<24 && p2=="yes"){
-    alert("you should have waited till 24")
-    
+
+
+for(let i = 0; i<Object.keys(marks).length;i++){
+    console.log(Object.keys(marks)[i]+"="+marks[Object.keys(marks)[i]])
 }
-    
-
-
-
-
-
-
-
-
