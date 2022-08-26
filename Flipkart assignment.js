00a1ade618dcd9ca6396e3bb92c92d222a66bf19
shaik sadiq <ssadiq.skt@gmail.com>
2022-08-27 00:25:08 +0530

    Assignment project on flipcart clone

---
+++ Flipkart assignment.js
@@ -0,0 +1,23 @@
+
+function mylogin (){
+    var un = document.forms["myform"]["Uname"].value;
+    var pw = document.forms["myform"]["pass"].value;
+    if (un== "sadiq" && pw== "1234") {
+      window.location.href="flipkartmain.html"
+    } else {
+      alert("wrong user name or password")
+    }
+ }
+
+
+
+ function mylogin1 (){
+    var un = document.forms["myform1"]["Uname1"].value;
+    var pw = document.forms["myform1"]["pass1"].value;
+    var cw = document.forms["myform1"]["cnpass1"].value;
+    if (un== "sadiq" && pw== "1234" &&  cw == pw) {
+      window.location.href="regsuccess.html"
+    } else {
+      alert("password and confirm password doesnt match")
+    }
+ }
